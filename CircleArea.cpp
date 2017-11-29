c9b291e34a483972c7cb2f65cfc1f070de4724fc
Nibesh Khadka <nibeshkhadka@gmail.com>
2017-11-29 21:40:12 +0200

    Area of circle with input radius

---
+++ CircleArea.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main(){
+
+								float radius;
+								float Pi=3.142;
+
+								cout <<"Input circle radius:";
+								cin>>radius;
+								float area= Pi*radius*radius;
+
+								cout<<"Circle area with the given radius: "<<area<<"\n";
+								return 0;
+}
